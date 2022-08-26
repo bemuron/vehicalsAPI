293702395e6e66949de62d9426982a7a1771ade3
bemuron <emo6bruno@gmail.com>
2022-08-26 17:26:14 +0300

    add pricing controller error responses

--- pricing-service/src/main/java/com/udacity/pricing/api/PricingController.java
+++ pricing-service/src/main/java/com/udacity/pricing/api/PricingController.java
@@ -4,6 +4,8 @@ import com.udacity.pricing.domain.price.Price;
 import com.udacity.pricing.service.PriceException;
 import com.udacity.pricing.service.PricingService;
 import org.springframework.http.HttpStatus;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +16,15 @@ import org.springframework.web.server.ResponseStatusException;
  * Implements a REST-based controller for the pricing service.
  */
 @RestController
+@ApiResponses(value = {
+        @ApiResponse(code=400, message = "This is a bad request, please follow the API documentation for the proper request format."),
+        @ApiResponse(code=401, message = "Due to security constraints, your access request cannot be authorized. "),
+        @ApiResponse(code=404, message = "Resource not found. You may have mistyped the address or the page may have moved."),
+        @ApiResponse(code=500, message = "The server is down. Please make sure that the Location microservice is running."),
+        @ApiResponse(code=503, message = "The server is unable to service your request due to maintenance downtime."),
+        @ApiResponse(code=505, message = "Forbidden. Something is broken. We have been a alerted and are working on it.")
+
+})
 @RequestMapping("/services/price")
 public class PricingController {
 
